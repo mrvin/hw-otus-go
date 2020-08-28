0baddbdd5554c1c381372c99c30356f00167cf28
mr.Vin <v.v.vinogradovv@gmail.com>
2020-08-28 15:33:17 +0300

    hw01: print current local time and exact time

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -1,5 +1,34 @@
+// A program that prints the current local time
+// and the exact time obtained using the NTP library
+// (github.com/beevik/ntp) in the format:
+// 		current time: <time>
+// 		exact time: <time>
 package main
 
+import (
+	"flag"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/beevik/ntp"
+)
+
+// Default ntp server name.
+var srvNTP = "2.ru.pool.ntp.org"
+
 func main() {
-	// Place your code here
+	flag.StringVar(&srvNTP, "n", srvNTP, "ntp server name")
+	flag.Parse()
+	log.Printf("server name: %s\n", srvNTP)
+
+	currentTime := time.Now().Round(0)
+	fmt.Printf("current time: %v\n", currentTime)
+
+	exactTime, err := ntp.Time(srvNTP)
+	if err != nil {
+		log.Fatalf("ntp.Time: %s", err)
+	}
+	exactTime = exactTime.Round(0)
+	fmt.Printf("exact time: %s\n", exactTime)
 }
