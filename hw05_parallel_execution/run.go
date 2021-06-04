0245d19599f2b7a786d625a948dd6bc3f7a7a083
mr.Vin <v.v.vinogradovv@gmail.com>
2021-06-04 15:57:18 +0300

    hw05: disable linter 'cyclop'
    Warns about calculated cyclomatic complexity for function Run is 11,
    max is 10

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -16,7 +16,7 @@ var (
 type Task func() error
 
 // Run starts tasks in N goroutines and stops its work when receiving M errors from tasks.
-func Run(tasks []Task, N int, M int) error { //nolint:gocritic
+func Run(tasks []Task, N int, M int) error { //nolint:gocritic,cyclop
 	taskCh := make(chan Task)
 	var errCnt int32
 
