ed618962d886b8ae7b44e4a732978d118e8aa66e
mr.Vin <v.v.vinogradovv@gmail.com>
2022-02-28 02:47:44 +0300

    hw01: refactoring test code

--- hw01hellonow/main.go
+++ hw01hellonow/main.go
@@ -1,4 +1,4 @@
-// A program that prints the current local time
+// That program prints the current local time
 // and the exact time obtained using the NTP library
 // (github.com/beevik/ntp) in the format:
 // 		current time: <time>
@@ -8,11 +8,26 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"log"
+	"os"
 	"time"
 
 	"github.com/beevik/ntp"
 )
+
+var out io.Writer = os.Stdout // modified during testing.
+
+var getTimeNTP = func(srvNTP string) (exactTime time.Time, err error) {
+	exactTime, err = ntp.Time(srvNTP)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+var getTimeNow = time.Now
 
 func main() {
 	var srvNTP string // ntp server name.
@@ -21,13 +36,14 @@ func main() {
 	flag.Parse()
 	log.Printf("server name: %s\n", srvNTP)
 
-	currentTime := time.Now().Round(0)
-	fmt.Printf("current time: %v\n", currentTime)
+	currentTime := getTimeNow().Round(0)
 
-	exactTime, err := ntp.Time(srvNTP)
+	exactTime, err := getTimeNTP(srvNTP)
 	if err != nil {
-		log.Fatalf("ntp.Time: %s", err)
+		log.Fatalf("getTimeNTP: %s", err)
 	}
 	exactTime = exactTime.Round(0)
-	fmt.Printf("exact time: %s\n", exactTime)
+
+	fmt.Fprintf(out, "current time: %v\n", currentTime)
+	fmt.Fprintf(out, "exact time: %s\n", exactTime)
 }
--- hw01hellonow/main_test.go
+++ hw01hellonow/main_test.go
@@ -1,70 +1,48 @@
 package main
 
 import (
-	"io/ioutil"
-	"os"
+	"bytes"
+	"io"
 	"testing"
 	"time"
-
-	"bou.ke/monkey"
-	"github.com/beevik/ntp"
 )
 
-// go test -gcflags=-l
-func TestHelloNow(t *testing.T) {
-	t.Run("test normal behavior", func(t *testing.T) {
-		layout := "2 Jan 2006 15:04:05"
-
-		monkey.Patch(time.Now, func() time.Time {
-			nowTime, err := time.Parse(layout, "9 May 1945 10:03:00")
-			if err != nil {
-				t.Fatal(err)
-			}
-			return nowTime
-		})
-
-		monkey.Patch(ntp.Time, func(_ string) (time.Time, error) {
-			ntpTime, err := time.Parse(layout, "9 May 1945 10:03:02")
-			if err != nil {
-				t.Fatal(err)
-			}
-			return ntpTime, nil
-		})
-
-		result, err := catchStdout(main)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		expected := `current time: 1945-05-09 10:03:00 +0000 UTC
-exact time: 1945-05-09 10:03:02 +0000 UTC
-`
-		if string(result) != expected {
-			t.Fatalf("invalid output:\n%s, expected:\n%s", result, expected)
-		}
-	})
+type saved struct {
+	out        io.Writer
+	getTimeNTP func(string) (time.Time, error)
+	getTimeNow func() time.Time
 }
 
-func catchStdout(runnable func()) (result []byte, err error) {
-	realOut := os.Stdout
-	defer func() { os.Stdout = realOut }()
+func TestHelloNow(t *testing.T) {
+	const layout = "2 Jan 2006 15:04:05"
+	const expected = "current time: 1945-05-09 10:03:00 +0000 UTC\nexact time: 1945-05-09 10:03:02 +0000 UTC\n"
 
-	r, fakeOut, err := os.Pipe()
-	if err != nil {
-		return
+	// Save and restore original: output, getTimeNTP, getTimeNow.
+	sevedAll := saved{out, getTimeNTP, getTimeNow}
+	defer func() {
+		out = sevedAll.out
+		getTimeNTP = sevedAll.getTimeNTP
+		getTimeNow = sevedAll.getTimeNow
+
+	}()
+
+	out = new(bytes.Buffer) // captured output
+
+	// Install the test's fake getTimeNTP.
+	getTimeNTP = func(srvNTP string) (time.Time, error) {
+		return time.Parse(layout, "9 May 1945 10:03:02")
 	}
 
-	os.Stdout = fakeOut
-	runnable()
-	if err = fakeOut.Close(); err != nil {
-		return
+	// Install the test's fake getTimeNow.
+	getTimeNow = func() time.Time {
+		currentTime, _ := time.Parse(layout, "9 May 1945 10:03:00")
+		return currentTime
 	}
 
-	result, err = ioutil.ReadAll(r)
-	if err != nil {
-		return
-	}
+	main()
 
-	err = r.Close()
-	return
+	got := out.(*bytes.Buffer).String()
+	if got != expected {
+		t.Fatalf("invalid output:\n%s, expected:\n%s", got, expected)
+	}
 }
