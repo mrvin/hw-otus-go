196bf7f6ce05e8aff8eb9f8dfbb7c537370ddc8d
mr.Vin <v.v.vinogradovv@gmail.com>
2022-04-12 21:11:25 +0300

    hw10: add parallel implementation GetDomainStat

--- hw10programoptimization/stats_optimization.go
+++ hw10programoptimization/stats_optimization.go
@@ -32,3 +32,64 @@ func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
 
 	return resultDomainStat, err
 }
+
+var numGoroutin = 4
+
+type result struct {
+	res string
+	err error
+}
+
+func GetDomainStatLongLivedGorout(r io.Reader, domain string) (DomainStat, error) {
+	content, err := ioutil.ReadAll(r)
+	if err != nil {
+		return nil, err
+	}
+	lines := bytes.Split(content, []byte{'\n'})
+	numLines := len(lines)
+
+	if numLines < numGoroutin {
+		numGoroutin = numLines
+	}
+	perGorout := numLines / numGoroutin
+	remGorout := numLines % numGoroutin
+	resultCh := make(chan *result, numGoroutin)
+
+	jh := new(codec.JsonHandle)
+	dotDomain := []byte("." + domain)
+	from, to := 0, 0
+	for i := 0; i < numGoroutin; i++ {
+		to += perGorout
+		if i < remGorout {
+			to++
+		}
+		go func(partLines [][]byte) {
+			var user user.User
+			for _, line := range partLines {
+				err = codec.NewDecoderBytes(line, jh).Decode(&user)
+				if err == nil {
+					byteEmail := []byte(user.Email)
+					if bytes.HasSuffix(byteEmail, dotDomain) {
+						resultCh <- &result{string(bytes.ToLower(bytes.SplitN(byteEmail, []byte{'@'}, 2)[1])), nil}
+						continue
+					}
+				}
+				resultCh <- &result{"", err}
+			}
+		}(lines[from:to])
+		from = to
+	}
+
+	resultDomainStat := make(DomainStat)
+	for i := 0; i < numLines; i++ {
+		res := <-resultCh
+		resultDomainStat[res.res]++
+		if res.err != nil {
+			err = res.err
+		}
+	}
+	delete(resultDomainStat, "")
+	close(resultCh)
+
+	return resultDomainStat, err
+}
--- hw10programoptimization/stats_optimization_test.go
+++ hw10programoptimization/stats_optimization_test.go
@@ -32,7 +32,7 @@ func TestGetDomainStat_Time_And_Memory(t *testing.T) {
 		require.NoError(t, err)
 
 		b.StartTimer()
-		stat, err := GetDomainStat(data, "biz")
+		stat, err := GetDomainStatLongLivedGorout(data, "biz")
 		b.StopTimer()
 		require.NoError(t, err)
 
