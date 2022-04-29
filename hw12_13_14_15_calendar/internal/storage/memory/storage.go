d71f97582df950ef6e1f3c5d0f41fe242b894b2e
mr.Vin <v.v.vinogradovv@gmail.com>
2022-04-29 12:31:16 +0300

    hw12: add function headers storage sql package

--- hw12_13_14_15_calendar/internal/storage/memory/storage.go
+++ hw12_13_14_15_calendar/internal/storage/memory/storage.go
@@ -1,6 +1,11 @@
 package memorystorage
 
-import "sync"
+import (
+	"context"
+	"sync"
+
+	"github.com/mrvin/hw-otus-go/hw12_13_14_15_calendar/internal/storage"
+)
 
 type Storage struct {
 	// TODO
@@ -11,4 +16,46 @@ func New() *Storage {
 	return &Storage{}
 }
 
-// TODO
+func (s *Storage) CreateEvent(ctx context.Context, event *storage.Event) error {
+	return nil
+}
+
+func (s *Storage) GetEvent(ctx context.Context, id int) (*storage.Event, error) {
+	return nil, nil
+}
+
+func (s *Storage) GetEventsForUser(ctx context.Context, id int) ([]*storage.Event, error) {
+	return nil, nil
+}
+
+func (s *Storage) GetListEvents(ctx context.Context) ([]*storage.Event, error) {
+	return nil, nil
+}
+
+func (s *Storage) UpdateEvent(ctx context.Context, event *storage.Event) error {
+	return nil
+}
+
+func (s *Storage) DeleteEvent(ctx context.Context, id int) error {
+	return nil
+}
+
+func (s *Storage) CreateUser(ctx context.Context, user *storage.User) error {
+	return nil
+}
+
+func (s *Storage) GetUser(ctx context.Context, id int) (*storage.User, error) {
+	return nil, nil
+}
+
+func (s *Storage) GetListUsers(ctx context.Context) ([]*storage.User, error) {
+	return nil, nil
+}
+
+func (s *Storage) UpdateUser(ctx context.Context, user *storage.User) error {
+	return nil
+}
+
+func (s *Storage) DeleteUser(ctx context.Context, id int) error {
+	return nil
+}
