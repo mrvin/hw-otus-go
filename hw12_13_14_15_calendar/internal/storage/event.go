d8ce030664245ae7f0c9c06b4d5e1450aed168fd
mr.Vin <v.v.vinogradovv@gmail.com>
2022-04-29 12:25:49 +0300

    hw12: add Event and User type

--- hw12_13_14_15_calendar/internal/storage/event.go
+++ hw12_13_14_15_calendar/internal/storage/event.go
@@ -1,7 +1,22 @@
 package storage
 
+import (
+	"time"
+)
+
 type Event struct {
-	ID    string
-	Title string
-	// TODO
+	ID          int
+	Title       string
+	Description string
+	StartTime   time.Time
+	StopTime    time.Time
+	UserID      int
+	//	CreatedAt   time.Time
+	//	UpdatedAt   time.Time
+}
+
+type User struct {
+	ID     int
+	Name   string
+	Events []*Event
 }
