b113b2f41062d322318f1c574aa826ad74c62937
mr.Vin <v.v.vinogradovv@gmail.com>
2022-04-29 11:11:52 +0300

    hw12: add implementation logger package

--- hw12_13_14_15_calendar/internal/logger/logger.go
+++ hw12_13_14_15_calendar/internal/logger/logger.go
@@ -1,11 +1,31 @@
 package logger
 
+import (
+	"log"
+	"os"
+
+	"github.com/mrvin/hw-otus-go/hw12_13_14_15_calendar/internal/config"
+)
+
 type Logger struct {
-	// TODO
+	*log.Logger
+	logFile *os.File
 }
 
-func New(level string) *Logger {
-	return &Logger{}
+func Create(conf *config.LoggerConf) (*Logger, error) {
+	logFile := os.Stdout
+	if conf.FilePath != "" {
+		var err error
+		logFile, err = os.OpenFile(conf.FilePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+		if err != nil {
+			return nil, err
+		}
+	}
+	logger := Logger{log.New(logFile, "", log.LstdFlags|log.Lshortfile), logFile}
+
+	return &logger, nil
 }
 
-// TODO
+func (l *Logger) Close() {
+	l.logFile.Close()
+}
