d1736c8d00a7484657fd86d099e5d595b893873e
mr.Vin <v.v.vinogradovv@gmail.com>
2022-05-16 12:11:39 +0300

    migrations: add email for user and time zone for event

--- hw12-15calendar/migrations/setup.sql
+++ hw12-15calendar/migrations/setup.sql
@@ -2,15 +2,16 @@ CREATE DATABASE event-db;
 
 CREATE TABLE users (
 	id serial primary key,
-	name text
+	name text,
+	email text
 );
 
 CREATE TABLE events (
 	id serial primary key,
 	title text,
 	description text,
-	start_time timestamp,
-	stop_time timestamp,
+	start_time timestamptz,
+	stop_time timestamptz,
 	user_id integer references users(id) on delete cascade
 --	send_time bigint,
 --	created_at timestamp,
