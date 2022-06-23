c281ace1d80a622ca17c1293632db6dc1d0e80f0
mr.Vin <v.v.vinogradovv@gmail.com>
2022-06-23 14:29:06 +0300

    hw12: create Makefile for calendar and rename config.yml

--- hw12-15calendar/deployments/start.sh
+++ hw12-15calendar/deployments/start.sh
@@ -5,12 +5,14 @@ set -e
 if [ "$ENV" = 'DEV' ]; then
 	# Есть возможность изменять код сервера без пересборки образа docker
 	echo "Running Development Server"
+	cd cmd/calendar/
 	exec make run
 elif [ "$ENV" = 'UNIT' ]; then
 	echo "Running Unit Tests"
+	cd cmd/calendar/
 	exec make test
 else
 	# Запуск сервера собраного при сборке образа docker
 	echo "Running Production Server"
-	exec ./bin/calendar -config "configs/config.yml"
+	exec ./bin/calendar -config "configs/calendar.yml"
 fi
