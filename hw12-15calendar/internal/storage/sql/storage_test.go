e9d244a9956958f3cfc5560eaec461aa65cf9172
mr.Vin <v.v.vinogradovv@gmail.com>
2022-12-01 17:10:52 +0300

    hostname postgres -> localhost

--- hw12-15calendar/internal/storage/sql/storage_test.go
+++ hw12-15calendar/internal/storage/sql/storage_test.go
@@ -9,7 +9,7 @@ import (
 
 var ctx = context.Background()
 
-var confDBTest = Conf{"postgres", 5432, "event-db", "event-db", "event-db"}
+var confDBTest = Conf{"localhost", 5432, "event-db", "event-db", "event-db"}
 
 func TestUserCRUD(t *testing.T) {
 	st, err := New(ctx, &confDBTest)
